0ac7197b4479157d8f5e7b4ab88c42a668fbf76b
JulianSwiatly <62251523+JulianSwiatly@users.noreply.github.com>
2020-03-22 17:42:58 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -29,7 +29,13 @@ def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
     """
     
     # Your code goes here (remove pass)
-    pass
+    CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
+    date = datetime.date(year, month, day)
+    date1 = datetime.date(year, month, day).strftime('%-m/%-d/%-y')
+    date2 = datetime.date(year, month, day) + datetime.timedelta(days=-1)
+    date2 = date2.strftime('%-m/%-d/%-y')
+    return len(confirmed_cases.loc[confirmed_cases[date2] != confirmed_cases[date1]])
 
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
